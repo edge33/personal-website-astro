5f4aaccf2167ca17d9734b2bb761cb1b361e0028
Francesco Maida <francescogithub@efem.dev>
2025-05-22 17:44:59 +0200

    fix: sitemap with host

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -16,6 +16,7 @@ export async function GET() {
     return new Response(
         `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url><loc>${host}</loc></url>
       ${urls.join('\n')}
       ${new Date().toString()}
     </urlset>`,
