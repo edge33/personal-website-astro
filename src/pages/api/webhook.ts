4e795d610ef4f1c5cbb2279aca945ac8835b4890
Francesco Maida <francesco.maida@nearform.com>
2025-05-27 20:45:20 +0200

    fix: fix response webtoken

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,14 +1,22 @@
 import type { APIRoute } from 'astro';
 import plainwhiteConfig from '../../plainwhite.config.ts';
 
-const return404 = () => {
-    return new Response(null, {
-        status: 404,
+// const return404 = () => {
+//     return new Response(null, {
+//         status: 404,
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     });
+// };
+
+const return200 = () =>
+    new Response(JSON.stringify({ hello: 'world' }), {
+        status: 200,
         headers: {
             'Content-Type': 'application/json',
         },
     });
-};
 
 export const POST: APIRoute = async ({ request }) => {
     const body = await request.json();
@@ -23,22 +31,18 @@ export const POST: APIRoute = async ({ request }) => {
             console.log(
                 `New notion token received: ${postedVerificationToken}, please update env vars`
             );
+        } else {
+            console.log(`Notion token verification request received`);
         }
-        console.log(`Notion token verification request received`);
-        return return404();
+        return return200();
     }
 
     const signature = request.headers.get('X-Notion-Signature');
 
     if (!signature) {
         console.log('Signature not present, ignoring the request');
-        return return404();
+        return return200();
     }
 
-    return new Response(JSON.stringify({ hello: 'world' }), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+    return return200();
 };
