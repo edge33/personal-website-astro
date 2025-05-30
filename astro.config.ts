4e77a553d2a314b3827a6f59563775fd26e90d93
Francesco Maida <francesco.maida@nearform.com>
2025-05-30 15:21:40 +0200

    chore: removed cache invalidation

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     output: 'server',
     adapter: vercel({
         isr: {
-            expiration: Number(CACHE_DURATION) || 60 * 62 * 24,
+            expiration: Number(CACHE_DURATION) || 60 * 60 * 24,
             bypassToken: CACHE_BYPASS_TOKEN,
             exclude: [
                 /^\/api\/.+/, // Regular expressions supported since @astrojs/vercel@v8.1.0
